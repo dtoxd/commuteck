23693dd33f203338dfce010a575339957a0ceefd
dtoxd <42573173+dtoxd@users.noreply.github.com>
2018-11-11 21:17:07 +0530

    Add files via upload

---
+++ nav/index.js
@@ -0,0 +1,11 @@
+function openNav() 
+{
+	document.getElementById("myNav").style.height = "100%";
+	document.getElementById("hd-w-nav").style.display = "none";
+}
+
+function closeNav() 
+{
+	document.getElementById("myNav").style.height = "0%";
+	document.getElementById("hd-w-nav").style.display = "block";
+}
